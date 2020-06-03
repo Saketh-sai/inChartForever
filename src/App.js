def9de124bcc58bb5637686d8de416031323fa6e
Thatikonda <thatiks@apac.corpdir.net>
2020-06-03 20:48:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Sweety extends React.Component {
+
+  render() {
+    return ( 
+      <p> Hello </p>
+    );
+  }
+}
+
+export default Sweety;
